aa77e987692ac66f8ba4ea853cd1f9d890094031
O. Inha <50331907+olaviinha@users.noreply.github.com>
2023-12-31 21:49:11 +0200

    rtmidi v

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ REQUIRED_PACKAGES = [
     'Pillow == 9.2.0',
     'pretty_midi == 0.2.9',
     'pygtrie == 2.5.0',
-    'python-rtmidi == 1.1.2',
+    'python-rtmidi == 1.5.8',
     'scikit-image == 0.19.3',
     'scipy == 1.7.3',
     'six == 1.16.0',
